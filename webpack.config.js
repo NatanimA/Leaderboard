2525ac9d64188065e84bdd1d80006841f91cc1de
natanimabesha <natanimabesha@www.zhewutech.com>
2022-10-11 15:09:59 +0300

    Fix linters

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,32 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        index: './src/js/index.js',
-    },
-    devServer: {
-        static: './dist',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
+  entry: {
+    index: './src/js/index.js',
+  },
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'index.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/Leaderboard/dist',
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    output: {
-        filename: 'index.bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/Leaderboard/dist',
-        clean: true,
-    },
 
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-
-    },
-    mode: 'development',
+  },
+  mode: 'development',
 };
